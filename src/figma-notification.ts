9cc92334c82df23eec7a49dbde65b73927b24e6c
masam0 <masaya.esp@gmail.com>
2021-07-12 14:25:09 +0900

    (WIP) Send FILE_COMMENT notification to Slack

--- src/figma-notification.ts
+++ src/figma-notification.ts
@@ -1,53 +1,60 @@
-import { GasWebClient as SlackClient } from '@hi-se/web-api';
+import {WebClient as SlackClient} from '@slack/web-api';
+import express from 'express'
 
-const TOKEN_SHEET_ID = '1ExiQonKpf2T8NnR9YFMzoD7jobHEuAXUUq3vaBL0hW8';
+const app: express.Express = express();
+
+// CORSの許可
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next()
+});
+
+// body-parserに基づいた着信リクエストの解析
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
 const FIGMA_EVENT_POST_CHANNEL = "C01AQPDC9S4" // #sysadm
 
-/*
-Webhookの送信元は現段階ではFigmaのみ
-まずはfigmaWebhookを受け取れるように設定を行う必要がある。
-それが終わったらGASの設定を行っていく。
-実はSlackのイベントは飛んでくる可能性は低く、特にboldをimportする必要はなさそう。
-Figmaからの通知が即時通知が必要なものであればpostMessageで内容をそのまま送信する。
-*/
 
-const isJson = (e: GoogleAppsScript.Events.DoPost) => {
-  return e.postData.type === 'application/json';
-}
-
-const isUrlVerification = (e: GoogleAppsScript.Events.DoPost) => {
-  if (isJson(e) && e.postData.contents) {
-    return (JSON.parse(e.postData.contents).type === 'url_verification');
+const isUrlVerification = (req) => {
+  if (req.body) {
+    return (req.body.event_type === 'PING');
   } else {
     return false;
   }
 }
 
-const init = () => {
-  const sheet = SpreadsheetApp.openById(TOKEN_SHEET_ID).getSheets()[0];
-  const row = sheet.getRange('A:A').createTextFinder('figma-notification').findNext().getRow();
-  const column = sheet.getRange(1, 1, 1, sheet.getLastColumn()).createTextFinder('Token').findNext().getColumn();
-  const slackToken = sheet.getRange(row, column).getValue();
-  PropertiesService.getScriptProperties().setProperty('SLACK_TOKEN', slackToken);
-}
-
-const getSlackClient = () => {
-  const token = PropertiesService.getScriptProperties().getProperty('SLACK_TOKEN');
+const slackClient = () => {
+  const token = process.env.BOT_TOKEN;
   return new SlackClient(token);
 }
 
 
-const doPost = (e: GoogleAppsScript.Events.DoPost) => {
-  console.info(`[doPost raw event]\n\n${JSON.stringify(e)}`);
-  if (isUrlVerification(e)) {
-    return ContentService.createTextOutput(JSON.parse(e.postData.contents)['challenge']);
+// Figmaのpayloadの処理
+const router: express.Router = express.Router()
+app.post('/', (req:express.Request, res:express.Response) => {
+  console.info(`[doPost raw event]\n\n${JSON.stringify(req.body)}`);
+
+  if(isUrlVerification(req)) {
+    return res.send(req.body.passcode);
   }
 
-  const client = getSlackClient();
-  return ContentService.createTextOutput('OK');
-}
+  const client = slackClient();
+  if(req.body.event_type === "FILE_COMMENT") {
+    const body = req.body;
+    client.chat.postMessage( {
+      channel: FIGMA_EVENT_POST_CHANNEL,
+      text: `
+${body.file_name}ファイルで${body.triggered_by.handle}さんよりコメントがありました。
+${body.comment.map(element => element.text).join("\n")}
+            `
+    })
+    return res.send('OK')
+  }
+});
+app.use(router)
 
-
-declare const global: any;
-global.doPost = doPost;
-global.init = init;
+app.listen(3000,()=>{ console.log('Example app listening on port 3000!') })
