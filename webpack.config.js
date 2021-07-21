be2ffedc979cb72c9651a40572682d371894435c
masam0 <masaya.esp@gmail.com>
2021-07-21 11:02:09 +0900

    Enable webpack build

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const { node } = require("webpack");
+// const GasPlugin = require("gas-webpack-plugin");
+
+const mode =
+  process.env.NODE_ENV === "development" ? "development" : "production";
+const devtool = "inline-source-map";
+const entry = path.resolve("src", "figma-notification.ts");
+const outPath = path.resolve("build");
+const outFileName = "backend.js";
+const output = {
+  path: outPath,
+  filename: outFileName,
+};
+const rules = [
+  {
+    test: /\.[tj]s$/,
+    use: {
+      loader: "ts-loader",
+      options: {
+        allowTsInNodeModules: true,
+        transpileOnly: true,
+        configFile: path.resolve("tsconfig.json"),
+      },
+    },
+  },
+];
+const resolve = {
+  extensions: [".ts", ".js", ".json"],
+  fallback: {
+    path: false,
+    os: false,
+  },
+};
+// const plugins = [new GasPlugin()];
+
+module.exports = [
+  {
+    mode: "development",
+    entry,
+    output,
+    module: { rules },
+    resolve,
+    target: "node", // added for nodejs
+    //plugins,
+  },
+];
